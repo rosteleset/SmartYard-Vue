652918c2e0a3a6de3a2f84076128758192437b02
preyai <preyaizman@gmail.com>
2024-03-01 14:21:14 +0300

    settings refactoring

--- src/components/AddressSettings.vue
+++ src/components/AddressSettings.vue
@@ -1,22 +1,30 @@
 <script setup lang="ts">
+// импорт необходимых модулей
 import { useSettings } from '../store/settings';
 import Switch from './Switch.vue';
 import dayjs from 'dayjs';
-import resetCode from '../api/resetCode'
+import resetCode from '../api/resetCode';
 import { computed } from 'vue';
 import Button from './Button.vue';
-import reloadIcon from '../assets/reload.svg'
+import reloadIcon from '../assets/reload.svg';
 import { ref } from 'vue';
 import { watch } from 'vue';
 
+// определение свойств
 const { flatId } = defineProps<{
     flatId: string
 }>()
 
+// использование настроек
 const { settings, save } = useSettings(flatId)
+
+// установка значения doorCode
 const doorCode = ref(settings.value?.doorCode)
+
+// вычисление значения isAutoOpen
 const isAutoOpen = computed(() => dayjs().isBefore(dayjs(settings.value?.autoOpen)))
 
+// функции для проверки настроек
 const isFRSSetting = (key: string) => {
     return key === 'FRSDisabled';
 };
@@ -25,86 +33,60 @@ const isWhiteRabbitSetting = (key: string) => {
     return key === 'whiteRabbit';
 };
 
+// функция обновления настроек
 const update = (key: string, value: string | boolean) => {
-    let _value = value
-
-    // инвертируем FRS
-    if (isFRSSetting(key))
-        _value = !value
-
-    let string: string
-    // конвертируем boolean в строку
-    switch (_value) {
-        case true:
-            // автооткрытие 5 минут
-            if (isWhiteRabbitSetting(key))
-                string = "5"
-            else
-                string = 't'
-            break;
-        case false:
-            // автооткрытие 5 минут
-            if (isWhiteRabbitSetting(key))
-                string = "0"
-            else
-                string = 'f'
-            break;
-        default:
-            string = _value
-            break;
-    }
+    const _value = isFRSSetting(key) ? !value : value;
+    const string = isWhiteRabbitSetting(key) ? (_value ? "5" : "0") : (_value ? 't' : 'f');
     save({ [key]: string });
-    // console.log({ [key]: string });
 }
 
+// функция для генерации нового кода
 const regenerateCode = () => {
     resetCode(flatId)
         .then(code => doorCode.value = code)
 }
 
+// функция для установки автоматического открытия
 const setAutoOpen = () => {
-    save({ autoOpen: dayjs().add(1, 'hour').format('YYYY-MM-DD HH:mm:ss') })
+    if (!isAutoOpen)
+        save({ autoOpen: dayjs().add(1, 'hour').format('YYYY-MM-DD HH:mm:ss') })
 }
 
-watch(settings, () => {
-    doorCode.value = settings.value?.doorCode
-})
+// отслеживание изменений в настройках
+watch(settings, (newSettings) => {
+    if (newSettings && newSettings.doorCode !== doorCode.value) {
+        doorCode.value = newSettings.doorCode;
+    }
+});
 
 </script>
 
 <template>
-
     <div class="grid">
         <h4>{{ $t('settings.intercom') }}</h4>
         <template v-if="settings?.CMS !== undefined">
             <div>{{ $t('settings.CMS') }}</div>
             <Switch :initial-value="settings.CMS === 't'" @update:model-value="update('CMS', $event)" />
         </template>
-
         <template v-if="settings?.VoIP !== undefined">
             <div>{{ $t('settings.VoIP') }}</div>
             <Switch :initial-value="settings.VoIP === 't'" @update:model-value="update('VoIP', $event)" />
         </template>
-
         <template v-if="settings?.FRSDisabled !== undefined">
             {{ $t('settings.FRSDisabled') }}
             <Switch :initial-value="settings.FRSDisabled === 'f'" @update:model-value="update('FRSDisabled', $event)" />
         </template>
-
         <template v-if="settings?.whiteRabbit !== undefined">
             <div>{{ $t('settings.whiteRabbit') }}</div>
             <Switch :initial-value="settings.whiteRabbit !== '0'" @update:model-value="update('whiteRabbit', $event)" />
         </template>
-
         <h4>{{ $t('settings.access') }}</h4>
-
         <template v-if="settings?.allowDoorCode === 't'">
             <div>
                 {{ $t('settings.reset-code') }} <strong>{{ doorCode }}</strong>
             </div>
             <button class="reset" @click="regenerateCode"><img :src="reloadIcon" alt=""></button>
         </template>
-
         <template v-if="settings?.autoOpen !== undefined">
             <div>
                 {{ $t('settings.guest-access') }}
@@ -133,7 +115,7 @@ watch(settings, () => {
     background: none;
     border: 0;
     cursor: pointer;
-    
+
     img {
         display: inline-block;
         transform: rotate(0deg);
@@ -141,13 +123,10 @@ watch(settings, () => {
     }
 
     &:active {
-        
         img {
             transition: transform 0s;
             transform: rotate(-360deg);
         }
     }
-
 }
 </style>
-
