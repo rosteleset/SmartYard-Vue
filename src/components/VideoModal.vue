f36d7fb636164ced2dd47e8a3aec418eb29cf61e
preyai <preyaizman@gmail.com>
2024-03-06 16:49:11 +0300

    second variant

--- src/components/VideoModal.vue
+++ src/components/VideoModal.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import { StyleValue, ref, watch } from "vue";
-import { getLiveURL, getPreviewURL, initializeVideoStream } from "../lib/video";
-import { useRanges } from "../store/ranges";
+import { StyleValue, onMounted, onUnmounted, ref, watch } from "vue";
+import {
+  getIframe,
+  getLiveURL,
+  getPreviewURL,
+  initializeVideoStream,
+} from "../lib/video";
 import { Camera, FormatedRange } from "../types/camera";
-
+import { relative } from "path";
+import { useRanges } from "../store/ranges";
 import RangeSelect from "./RangeSelect.vue";
 
 const props = defineProps<{
@@ -16,6 +21,7 @@ const props = defineProps<{
 const emit = defineEmits(["onClose"]);
 
 const isPlaying = ref(false);
+const isOpenInfo = ref(false);
 const previewElement = ref<HTMLVideoElement | null>(null);
 const videoElement = ref<HTMLVideoElement | null>(null);
 const styles = ref<StyleValue>();
@@ -28,179 +34,156 @@ const range = ref<FormatedRange>();
 watch(
   () => props.isOpen,
   () => {
-    // styles.value = props.startStyles;
-
+    styles.value = props.startStyles;
     if (!props.isOpen) isPlaying.value = false;
   }
 );
 
-const onLoadPreview = () => {
-  if (previewElement.value) {
+const resize = () => {
+  if (previewElement.value !== null) {
     response.value =
       previewElement.value.videoWidth / previewElement.value.videoHeight;
-    const width = 90 * response.value;
+
+    // Вычисляем соотношение сторон
+    const aspectRatio = response.value;
+
+    // Получаем размеры контейнера (например, размеры окна браузера)
+    const containerWidth = window.innerWidth;
+    const containerHeight = window.innerHeight;
+
+    // Вычисляем новые размеры видео
+    let newVideoWidth, newVideoHeight;
+
+    if (containerWidth / aspectRatio > containerHeight) {
+      newVideoWidth = containerHeight * 0.9 * aspectRatio;
+      newVideoHeight = containerHeight * 0.9;
+    } else {
+      newVideoWidth = containerWidth * 0.9;
+      newVideoHeight = (containerWidth * 0.9) / aspectRatio;
+    }
+
+    styles.value = {
+      top: `${(containerHeight - newVideoHeight) / 2}px`,
+      left: `${(containerWidth - newVideoWidth) / 2}px`,
+      width: `${newVideoWidth}px`, // Преобразуем в пиксели
+      height: `${newVideoHeight}px`, // Преобразуем в пиксели
+    };
   }
+};
+
+const onLoad = () => {
+  resize();
   if (videoElement.value)
     initializeVideoStream(getLiveURL(props.camera), videoElement.value);
 };
 
-const onLoadVideo = () => {
+const ready = () => {
   isPlaying.value = true;
-  // styles.value = {
-  //     top: 0,
-  //     left: 0,
-  //     width: "100vw",
-  //     height: "100vh",
-  //     opacity:1
-  //   };
-
-  //   setTimeout(() => {
-  //     styles.value = {
-  //       top: 0,
-  //       left: 0,
-  //       width: "100vw",
-  //       height: "100vh",
-  //       opacity:1,
-  //       backgroundColor: "rgba(0, 0, 0, 0.3)",
-  //     };
-  //   }, 1000);
 };
 
+onMounted(() => {
+  window.addEventListener("resize", resize);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", resize);
+});
+
 watch(range, () => {
-  if (videoElement.value && range.value)
+  isPlaying.value = false;
+  if (videoElement.value)
     initializeVideoStream(
-      getLiveURL(props.camera, range.value.from, range.value.duration),
+      getLiveURL(props.camera, range.value?.from, range.value?.duration),
       videoElement.value
     );
 });
 </script>
 
 <template>
-  <Transition name="fade">
-    <div
-      class="pop-up"
-      v-if="isOpen"
-      :style="styles"
-      v-on:click="emit('onClose')"
-    >
-      <div class="video-container" @click.stop>
-        <div class="video">
-          <video
-            ref="videoElement"
-            v-on:canplay="onLoadVideo"
-            :controls="range !== undefined"
-          ></video>
-        </div>
-          <div class="info">
-            <div class="info__name">{{ camera.name }}</div>
-            <RangeSelect
-              :streams="streams"
-              v-model="range"
-            />
-          </div>
-        <Transition name="fade">
-          <video
-            v-if="!isPlaying"
-            ref="previewElement"
-            class="preview"
-            :src="preview"
-            v-on:canplay="onLoadPreview"
-          ></video>
-        </Transition>
-      </div>
-      <!--  -->
+  <div class="pop-up" v-if="isOpen" v-on:click="emit('onClose')">
+    <div class="video-container" :style="styles" @click.stop>
+      <video ref="videoElement" v-on:canplay="ready" :controls="range != undefined"></video>
+      <video
+        ref="previewElement"
+        class="previewElement"
+        :class="{active:isPlaying}"
+        :src="preview"
+        v-on:canplay="onLoad"
+      />
 
-      <!-- </Transition> -->
-      <!-- <iframe :src="getIframe(camera)" :style="styles"></iframe> -->
+      <div class="info" :class="{ open: isOpenInfo }">
+        <button class="togle-info" @click="isOpenInfo = !isOpenInfo"><</button>
+        <div class="info__label">{{ camera.name }}</div>
+        <RangeSelect
+          v-if="streams.length > 0"
+          :streams="streams"
+          v-model:modelValue="range"
+        />
+      </div>
     </div>
-  </Transition>
+  </div>
 </template>
 
 <style scoped lang="scss">
 .pop-up {
   position: fixed;
+  transition: 1s;
   top: 0;
   left: 0;
   width: 100vw;
   height: 100vh;
-  transition: 1s;
   z-index: 100;
-  background-color: transparentize($color: #000000, $amount: 1);
+  background-color: transparentize($color: #000000, $amount: 0.7);
   display: flex;
   align-items: center;
   justify-content: center;
 
-  // video,
-  // iframe {
-  //   position: absolute;
-  //   max-width: 90vw;
-  // }
-}
-
-.video-container {
-  position: absolute;
-  max-width: 90vw;
-  overflow: auto;
-  transition: 1s;
-  display: flex;
-  align-items: stretch;
-  width: max-content;
-  background-color: #ffffff;
-  border-radius: 12px;
-  @media (max-width: 1024px) {
-    flex-direction: column;
-    align-items: center;
-  }
-  .video {
-  }
-
-  video {
-    display: block;
-    max-width: 100%;
-    max-height: 90vh;
-    flex: 0;
-    border-radius: 12px 0 0 12px;
-  }
-
-  .preview {
+  .video-container {
+    transition: 1s;
     position: absolute;
-    top: 0;
-    left: 0;
-    object-fit: cover;
-    height: 100%;
-    // width: 100%;
-  }
+    overflow: hidden;
+    border-radius: 12px;
 
-  .info {
-    background-color: #ffffff;
-    // flex: 1;
-    padding: 12px;
-    border-radius: 0 12px 12px 0;
-    width: min-content;
-    &__name {
-      font-size: 20px;
-      margin-bottom: 24px;
+    video {
+      display: block;
+      width: 100%;
+      height: 100%;
+    }
+    .previewElement {
+      position: absolute;
+      top: 0;
+      left: 0;
+      opacity: 1;
+      &.active {
+        opacity: 0;
+      }
     }
   }
 }
 
-.info-enter-active,
-.info-leave-active {
-  transition: opacity 1s ease;
+.togle-info {
+  position: absolute;
+  right: 100%;
+  top: 50%;
 }
 
-.info-enter-from,
-.info-leave-to {
-  opacity: 0;
-}
+.info {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: min-content;
+  padding: 12px;
+  background-color: #ffffff;
+  transform: translateX(100%);
+  transition: 0.5s;
+  &.open {
+    transform: translateX(0);
+  }
 
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 1s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
+  &__label {
+    font-size: 20px;
+    margin-bottom: 12px;
+  }
 }
 </style>
