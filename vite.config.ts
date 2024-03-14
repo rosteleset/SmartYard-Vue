f33cdbafe3046d17207530c3e553587ef56e1fac
preyai <preyaizman@gmail.com>
2024-03-14 13:22:06 +0300

    remove ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.DEPLOY_ENV === "gh-pages" ? "/SmartYard-Web/" : "/",
-  server: process.env.DEPLOY_ENV === "gh-pages" ? {} : {
+  server: {
     proxy: {
       "/api": {
         target: "https://rbt-demo.lanta.me",
