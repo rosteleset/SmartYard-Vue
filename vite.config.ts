1ef0c8d625d6fe303b74b88dbb95796ac4c73941
preyai <preyaizman@gmail.com>
2024-03-14 12:29:33 +0300

    add ghpages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.DEPLOY_ENV === "gh-pages" ? "/SmartYard-Web/" : "/",
   server: {
     proxy: {
       "/api": {
