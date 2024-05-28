4ea010c8b97d4e7744bd74261c0abcd58d8b978c
preyai <preyaizman@gmail.com>
2024-05-28 13:04:12 +0300

    Wip (tests)

---
+++ src/vitest-setup.ts
@@ -0,0 +1,54 @@
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import {vi} from "vitest";
+
+global.L = L;
+
+vi.mock("@/store/user", () => ({
+    useUserStore: () => ({
+        load: vi.fn(),
+        clients: [],
+        isLoaded: true,
+        isAuth: true,
+        token: '123',
+        setToken: vi.fn(),
+        logout: vi.fn()
+    }),
+}));
+
+vi.mock("@/store/addresses", () => ({
+    useAddressesStore: () => ({
+        isLoaded: true,
+        addresses: [],
+        load: vi.fn(),
+        getAddressByHouseId: vi.fn(),
+        getClientsByHouseId: vi.fn(),
+        getAddressByFlatId: vi.fn()
+    }),
+}));
+
+vi.mock("@/store/config", () => ({
+    useAddressesStore: () => ({
+        config: {},
+        notifications: {},
+        names: {},
+        updateConfig: vi.fn(),
+        getTheme: vi.fn(),
+        updateTheme: vi.fn(),
+        sendName: vi.fn()
+    }),
+}));
+
+vi.mock("@/store/config", () => ({
+    useAddressesStore: () => ({
+        config: {},
+        notifications: {},
+        names: {},
+        updateConfig: vi.fn(),
+        getTheme: vi.fn(),
+        updateTheme: vi.fn(),
+        sendName: vi.fn()
+    }),
+}));
+
+
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,14 @@
-import { mergeConfig, defineConfig } from "vitest/config";
+import {mergeConfig, defineConfig} from "vitest/config";
 import viteConfig from "./vite.config";
 
 export default defineConfig((env) =>
-  mergeConfig(
-    viteConfig(env),
-    defineConfig({
-      test: {
-        environment: "happy-dom",
-      },
-    })
-  )
+    mergeConfig(
+        viteConfig(env),
+        defineConfig({
+            test: {
+                environment: "happy-dom",
+                setupFiles: './src/vitest-setup.ts',
+            },
+        })
+    )
 );
