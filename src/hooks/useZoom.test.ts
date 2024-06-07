6cf4e41c776763db74d01a57419ddfc7e95e69c6
preyai <preyaizman@gmail.com>
2024-06-07 17:43:37 +0300

    WiP

---
+++ src/hooks/useZoom.test.ts
@@ -0,0 +1,22 @@
+import {describe, it} from "vitest";
+
+// vi.mock();
+
+describe("useZoom", () => {
+
+    it("should set initial values correctly", () => {
+
+    });
+
+    it("should handle scroll event correctly", () => {
+
+    });
+
+    it("should handle drag events correctly", () => {
+
+    });
+
+    it("should stop drag after mouseup event", () => {
+
+    });
+});
